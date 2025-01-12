37eb20881ca1ad26ed4d8d7456e95bc43bd7a798
Jason Jashari <13jasonja@gmail.com>
2025-01-12 17:58:41 +0000

    Added error handling for retrieving a board

--- backend/index.js
+++ backend/index.js
@@ -19,14 +19,36 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+// error handler middleware
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  // CastError: invalid object id for Mongo
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'invalid id' })
+  }
+
+  // else, pass error to default express error handler
+  next(error)
+}
+
 app.get('/api/boards', (request, response) => {
   Board.find({}).then(boards => response.json(boards))
 })
 
-app.get('/api/boards/:id', (request, response) => {
+app.get('/api/boards/:id', (request, response, next) => {
   const id = request.params.id
-  Board.findById(id).then(board => response.json(board))
-  // error response.status(404).end()
+  Board.findById(id)
+    .then(board => {
+      // if no matching object in db, board will be null
+      if (board) {
+        response.json(board)
+      } else {
+        response.status(404).end()
+      }
+    })
+    // execution continues to the error handler middleware
+    .catch(error => next(error))
 })
 
 app.delete('/api/boards/:id', (request, response) => {
@@ -50,8 +72,12 @@ app.post('/api/boards', (request, response) => {
   board.save().then(savedBoard => response.json(savedBoard))
 })
 
-// Middleware to catch requests made to non-existing routes
+// middleware to catch requests made to non-existing routes
 app.use(unknownEndpoint)
+
+// last loaded middlware
+// invoked when error is thrown
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
