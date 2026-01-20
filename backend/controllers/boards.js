d30dd0f2f4583795e14267ebb0ec86b4d9ac9674
Jasoncls <13jasonja@gmail.com>
2026-01-20 17:50:57 +0000

    Added error handling for incorrect board id

--- backend/controllers/boards.js
+++ backend/controllers/boards.js
@@ -5,7 +5,10 @@ const { Board } = require('../models')
 // Middleware
 // Retrieve board from database and place it in the "board" property of the req object
 const boardFinder = async (req, res, next) => {
-    req.board = await Board.findByPk(req.params.id)
+    req.board = await Board.findByPk(req.params.id).catch((err) => {
+        console.log(err)
+        res.status(400).json({ error: 'Invalid board id' })
+    })
     next()
 }
 
