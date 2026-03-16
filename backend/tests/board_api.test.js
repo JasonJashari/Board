6ec1b8a772e05d6f02cf6ff63b550e3ab5ab812b
Jasoncls <13jasonja@gmail.com>
2026-03-16 13:29:06 +0000

    Initialise database before running tests

--- backend/tests/board_api.test.js
+++ backend/tests/board_api.test.js
@@ -1,10 +1,32 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach } = require("node:test");
 const assert = require("node:assert");
 const { sequelize } = require("../utils/db");
 const supertest = require("supertest");
 const app = require("../app");
+const Board = require("../models/board");
 
 const api = supertest(app);
+
+const initialBoards = [
+  {
+    title: "HTML",
+    content: "HTML is easy",
+  },
+  {
+    title: "Browsers",
+    content: "Browser can execute only JavaScript",
+  },
+];
+
+beforeEach(async () => {
+  await Board.destroy({
+    where: {}, // delete all rows
+  });
+  const b1 = Board.build(initialBoards[0]);
+  await b1.save();
+  const b2 = Board.build(initialBoards[1]);
+  await b2.save();
+});
 
 test("boards are returned as json", async () => {
   await api
@@ -16,7 +38,7 @@ test("boards are returned as json", async () => {
 test("all boards are returned", async () => {
   const response = await api.get("/api/boards");
 
-  assert.strictEqual(response.body.length, 2);
+  assert.strictEqual(response.body.length, initialBoards.length);
 });
 
 test("a specific board is within the returned boards", async () => {
